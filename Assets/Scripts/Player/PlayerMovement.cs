f2ddb756493453dbb8a20f408429365e2bb25d94
Adrian <adrianmurielzamora@gmail.com>
2023-02-01 20:19:07 +0100

    basic player animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,16 +12,21 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
+    private Animator anim;
     private Rigidbody2D rb;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        anim.SetFloat("x_speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        anim.SetBool("Jumping", !IsGrounded());
+        anim.SetBool("Grounded", IsGrounded());
 
         if (Input.GetButtonDown("Jump") && (IsGrounded() || jumpNumber < 2))
         {
@@ -50,6 +55,8 @@ public class PlayerMovement : MonoBehaviour
         if (hit.collider != null)
         {
             jumpNumber = 0;
+            anim.SetBool("Jumping", false);
+            anim.SetBool("Grounded", true);
             return true;
         }
 
